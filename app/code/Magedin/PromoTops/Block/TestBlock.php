729677bdf55b4dc382ef0eaf43a0e66b6984db49
Augusto Alves da Silva <augustoalvess@hotmail.com>
2020-12-17 18:28:35 -0300

    Ajustando

---
+++ app/code/Magedin/PromoTops/Block/TestBlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MagedIn\PromoTops\Block;
+
+class TestBlock extends \Magento\Framework\View\Element\Template
+{
+    public function getText()
+    {
+        return 'Texto do block';
+    }
+}
